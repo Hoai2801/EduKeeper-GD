3e66829fe9307fec5e88ce4f5f550cee3a5b3745
Hoai2801 <dreamhoaihack@gmail.com>
2024-02-05 20:19:07 +0700

    add devtool

--- backend/src/main/java/com/GDU/backend/controllers/HomeController.java
+++ backend/src/main/java/com/GDU/backend/controllers/HomeController.java
@@ -4,12 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Year;
+import java.util.Calendar;
+import java.util.Date;
+
 @RestController
 @RequestMapping("api/v1/home")
 public class HomeController {
 
-    @GetMapping("/")
+    // for benmarking 
+    @GetMapping
     public String home() {
-        return "Welcome to Backend";
+        return String.format("Today is " + Calendar.DATE + "/" + Calendar.MONTH + "/" + Year.now().getValue());
     }
 }
